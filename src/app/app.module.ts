6244d2d691ecc571295a2c18166eb7d747ede170
Jose Camarena <jose.j.camarena@gmail.com>
2023-08-26 18:17:48 -0700

    updated all features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './users/register/register.component';
+import { ViewuserComponent } from './users/viewuser/viewuser.component';
+import { UpdateuserComponent } from './users/updateuser/updateuser.component';
+import { DeleteuserComponent } from './users/deleteuser/deleteuser.component';
+import { LandingComponent } from './home/landing/landing.component';
+import { AppheaderComponent } from './appheader/appheader.component';
+import { AppfooterComponent } from './appfooter/appfooter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ViewuserComponent,
+    UpdateuserComponent,
+    DeleteuserComponent,
+    LandingComponent,
+    AppheaderComponent,
+    AppfooterComponent
   ],
   imports: [
     BrowserModule
